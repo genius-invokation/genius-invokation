0c60fc7889a6cdf041471229d7016e5f129f67f3
Guyutongxue <guyutongxue@163.com>
2025-01-24 14:32:55 +0800

    update

--- packages/card-data-img-gen/src/main.ts
+++ packages/card-data-img-gen/src/main.ts
@@ -1,62 +1,76 @@
-import puppeteer from "puppeteer-core";
+import puppeteer, { Page, type LaunchOptions } from "puppeteer-core";
+import { Cluster } from "puppeteer-cluster";
 import { createServer } from "vite";
 import path from "node:path";
 import { characters, actionCards } from "@gi-tcg/static-data";
+import { SingleBar, Presets } from "cli-progress";
+
+const PORT = 1337;
+const WORKSPACE_PATH = path.resolve(import.meta.dirname, "..");
+const OUTPUT_PATH = path.resolve(WORKSPACE_PATH, "dist");
+const CONCURRENCY = 4;
+
+console.log("Starting Vite server...");
+const server = await createServer({
+  root: WORKSPACE_PATH,
+  configFile: path.resolve(WORKSPACE_PATH, "vite.config.ts"),
+  server: {
+    port: PORT,
+  },
+});
+await server.listen();
+const url = server.resolvedUrls!.local[0]!;
 
 let executablePath = process.env.BROWSER_PATH;
 
 if (!executablePath) {
   const { install, Browser } = await import("@puppeteer/browsers");
+  console.log("Installing browser...");
+  const progress = new SingleBar({}, Presets.shades_classic);
   executablePath = (
     await install({
       browser: Browser.CHROME,
       buildId: "132.0.6834.83",
       cacheDir: "temp",
+      downloadProgressCallback: (curr, total) => {
+        progress.setTotal(total);
+        progress.update(curr);
+      },
     })
   ).executablePath;
+  progress.stop();
 }
 
-const browser = await puppeteer.launch({
-  executablePath,
-  defaultViewport: {
-    width: 480,
-    height: 640,
-  },
-  headless: false,
+console.log("Launching puppeteer...");
+const cluster: Cluster<void, void> = await Cluster.launch({
+  concurrency: Cluster.CONCURRENCY_PAGE,
+  maxConcurrency: CONCURRENCY,
+  puppeteer,
+  timeout: 60_000,
+  puppeteerOptions: {
+    executablePath,
+    defaultViewport: {
+      width: 480,
+      height: 640,
+    },
+    headless: false,
+    devtools: true,
+  } satisfies LaunchOptions,
 });
 
-const page = await browser.newPage();
-
-const PORT = 1337;
-const WORKSPACE_PATH = path.resolve(import.meta.dirname, "..");
-const OUTPUT_PATH = path.resolve(WORKSPACE_PATH, "dist");
-
-const server = await createServer({
-  root: WORKSPACE_PATH,
-  configFile: path.resolve(WORKSPACE_PATH, "vite.config.ts"),
-  server: {
-    port: 1337,
-  },
-});
-await server.listen();
-
-await page.goto(server.resolvedUrls!.local[0]!);
-
-async function getCharacter(id: number) {
+async function getCharacter(page: Page, id: number) {
   await page.evaluate((id) => window.showCharacter(id), id);
   await page.waitForNetworkIdle();
   await page.evaluate(() =>
     document.querySelectorAll("details").forEach((e) => (e.open = true)),
   );
-  await page.waitForNetworkIdle();
   const buf = await page
     .$("#root")
     .then((e) => e!.screenshot({ quality: 100, type: "webp" }));
   await Bun.write(path.resolve(OUTPUT_PATH, `${id}.webp`), buf);
 }
 
-
-async function getCard(id: number) {
+async function getCard(page: Page, id: number) {
   await page.evaluate((id) => window.showCard(id), id);
   await page.waitForNetworkIdle();
   const buf = await page
@@ -65,10 +79,27 @@ async function getCard(id: number) {
   await Bun.write(path.resolve(OUTPUT_PATH, `${id}.webp`), buf);
 }
 
-for (const ch of characters) {
-  await getCharacter(ch.id);
-}
-for (const ac of actionCards) {
-  await getCard(ac.id);
+const tasks = [
+  ...characters.map((ch) => (page: Page) => getCharacter(page, ch.id)),
+  // ...actionCards.map((ac) => (page: Page) => getCard(page, ac.id))
+];
+
+console.log("Running tasks...");
+const progress = new SingleBar({}, Presets.shades_classic);
+progress.start(tasks.length, 0);
+
+cluster.on('taskerror', (err) => {
+  console.log(`Error: ${err.message}`);
+});
+
+for (const task of tasks) {
+  cluster.queue(async ({ page }) => {
+    await page.goto(url);
+    await task(page);
+    progress.increment();
+  });
 }
 
+progress.stop();
+await cluster.idle();
+await cluster.close();
