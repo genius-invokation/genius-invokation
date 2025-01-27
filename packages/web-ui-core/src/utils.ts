97ce415d48334524b962c6e26517afbdf5eb8219
Guyutongxue <guyutongxue@163.com>
2025-01-27 15:55:07 +0800

    remove postcss from all components

---
+++ packages/web-ui-core/src/utils.ts
@@ -0,0 +1,49 @@
+// Copyright (C) 2024-2025 Guyutongxue
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { createSignal, Accessor } from "solid-js";
+
+export function createWaitNotify<T = unknown>(): [
+  waiting: Accessor<boolean>,
+  wait: () => Promise<T>,
+  notify: (value: T) => void,
+] {
+  const [waiting, setWaiting] = createSignal(false);
+  let resolve: (value: T) => void = () => {};
+  const wait = () => {
+    setWaiting(true);
+    return new Promise<T>((r) => {
+      resolve = r;
+    });
+  };
+  const notify = (value: T) => {
+    setWaiting(false);
+    return resolve(value);
+  };
+  return [waiting, wait, notify];
+}
+
+export function groupBy<T, K>(list: T[], getKey: (item: T) => K): Map<K, T[]> {
+  return list.reduce((result, item) => {
+    const key = getKey(item);
+    const collection = result.get(key);
+    if (!collection) {
+      result.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+    return result;
+  }, new Map<K, T[]>());
+}
--- packages/web-ui-core/unocss.config.ts
+++ packages/web-ui-core/unocss.config.ts
@@ -1,28 +1,35 @@
 // Copyright (C) 2024-2025 Guyutongxue
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import UnoCSS from "@unocss/postcss";
+import { defineConfig, presetUno } from "unocss";
+import { parseColor, colorToString } from "@unocss/preset-mini/utils";
 
-/** @type {import("postcss-load-config").Config} */
-export default {
-  plugins: [UnoCSS()],
-  // https://github.com/unocss/unocss/discussions/3444
-  postprocess: [
-    (obj) => {
-      obj.selector = ".gi-tcg-deck-builder " + obj.selector;
-    },
+export default defineConfig({
+  presets: [presetUno()],
+  rules: [
+    [
+      /^btn-(.*)$/,
+      ([, c], { theme }) => {
+        const data = parseColor(c, theme, "colors");
+
+        if (data?.cssColor) {
+          return {
+            "--btn-color": colorToString(data.cssColor),
+          };
+        }
+      },
+    ],
   ],
-
-};
+});
