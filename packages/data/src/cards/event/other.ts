d2c37923545cf9ae2121ecb3040ccbf917f6e7fc
Guyutongxue <guyutongxue@163.com>
2025-02-25 15:37:24 +0800

    fix(data): 野猪公主

--- packages/data/src/cards/event/other.ts
+++ packages/data/src/cards/event/other.ts
@@ -889,10 +889,7 @@ export const [TheBoarPrincess] = card(332025)
   .on("dispose", (c, e) => e.entity.definition.type === "equipment")
   .generateDice(DiceType.Omni, 1)
   .consumeUsage()
-  .on("enter", (c, e) => e.overridden &&
-    e.entity.definition.type === "equipment" && (
-    e.entity.definition.tags.includes("weapon") ||
-    e.entity.definition.tags.includes("artifact")))
+  .on("enter", (c, e) => e.overridden && e.entity.definition.type === "equipment")
   .generateDice(DiceType.Omni, 1)
   .consumeUsage()
   .done();
