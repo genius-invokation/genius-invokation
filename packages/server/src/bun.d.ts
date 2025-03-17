136b19ba729d97001256a0db9d0e883f59b761b0
Guyutongxue <guyutongxue@163.com>
2025-03-17 14:34:04 +0800

    add Dockerfile

--- packages/server/src/bun.d.ts
+++ packages/server/src/bun.d.ts
@@ -2,3 +2,5 @@ declare module "@gi-tcg/web-client" {
   declare const map: Record<string, string>;
   export default map;
 }
+
+declare const __LATEST_GIT_LOG__: import("simple-git").LogResult | undefined;
