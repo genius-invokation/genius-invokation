43aa652c3befce6cc2e716998972f6322db88012
DrAbx123 <79169815+DrAbx123@users.noreply.github.com>
2025-05-18 11:33:29 +0800

    fix(data): keqing liightningstiletto talent (#255)
    * keqing liightningstiletto
    
    * .
    
    * Update keqing.ts
    
    * Update keqing.ts

--- packages/data/src/characters/electro/keqing.ts
+++ packages/data/src/characters/electro/keqing.ts
@@ -1,15 +1,15 @@
 // Copyright (C) 2024-2025 Guyutongxue
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -125,7 +125,7 @@ export const LightningStiletto = card(114031)
   .since("v3.3.0")
   .unobtainable()
   .costElectro(3)
-  .tags("action")
+  .tags("action", "talent")
   .addTarget(`my character with definition id ${Keqing} and not has status with tag (disableSkill)`)
   .switchActive(`@targets.0`)
   .useSkill(StellarRestoration)
