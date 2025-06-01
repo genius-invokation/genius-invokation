acc055cb242e5ee7e2ad732a476d910ebab0b2d2
DrAbx123 <79169815+DrAbx123@users.noreply.github.com>
2025-06-02 05:31:20 +0800

    revert beidou.ts

--- packages/data/src/characters/electro/beidou.ts
+++ packages/data/src/characters/electro/beidou.ts
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { CardDefinition } from "@gi-tcg/core";
 import { character, skill, status, combatStatus, card, DamageType, DiceType } from "@gi-tcg/core/builder";
 
 /**
@@ -42,10 +41,10 @@ export const Wavestrider = skill(14054)
   .prepared()
   .do((c) => {
     c.$(`status with definition id ${TidecallerSurfEmbrace} at @self`)?.dispose();
-    c.damage(DamageType.Electro, 3);
-    if (c.self.hasEquipment(LightningStorm)){
-      c.characterStatus(SummonerOfLightning, "@self")
-    }
+  }
+  .damage(DamageType.Electro, 3)
+  .if((c) => c.self.master.hasEquipment(LightningStorm))
+  .characterStatus(SummonerOfLightning, "@master")
   })
   .done();
 
