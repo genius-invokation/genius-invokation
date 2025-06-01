06ff8620e77e9542062ff9d5e37b30db900eb185
Guyutongxue <guyutongxue@163.com>
2025-06-01 19:23:43 +0800

    fix(core): plunging attack (#303)

--- packages/test/__tests__/plunging.test.tsx
+++ packages/test/__tests__/plunging.test.tsx
@@ -16,9 +16,30 @@
 import { ref, setup, State, Card, Support, Character, Equipment, Summon, DeclaredEnd } from "#test";
 import { SkillHandle } from "@gi-tcg/core/builder";
 import { PlungingStrike } from "@gi-tcg/data/internal/cards/event/other";
-import { Albedo, DescentOfDivinity, FavoniusBladeworkWeiss, SolarIsotoma } from "@gi-tcg/data/internal/characters/geo/albedo";
+import { Keqing } from "@gi-tcg/data/internal/characters/electro/keqing";
+import { AbiogenesisSolarIsotoma, Albedo, DescentOfDivinity, FavoniusBladeworkWeiss, SolarIsotoma } from "@gi-tcg/data/internal/characters/geo/albedo";
 import { BiteyShark, Mualani } from "@gi-tcg/data/internal/characters/hydro/mualani";
 import { test } from "bun:test";
+
+test("plunging : negative test", async () => {
+  const target = ref();
+  const albedo = ref();
+  const c = setup(
+    <State>
+      <DeclaredEnd opp />
+      <Character opp ref={target} />
+      <Character my active />
+      <Character my def={Albedo} ref={albedo}>
+        <Equipment def={DescentOfDivinity} />
+      </Character>
+    </State>,
+  );
+  await c.me.switch(Albedo);
+  await c.me.skill(AbiogenesisSolarIsotoma);
+  await c.me.skill(FavoniusBladeworkWeiss);
+  // 由于不是切人后的第一个快速行动，所以不触发下落攻击
+  await c.expect(target).toHaveVariable({ health: 8 });
+});
 
 test("plunging triggered by a in-skill-switch to Albedo", async () => {
   const target = ref();
