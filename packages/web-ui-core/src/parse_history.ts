b5282f109f7fba8bd358f5e9fa6ee47621f42245
Guyutongxue <guyutongxue@163.com>
2025-06-16 16:44:48 +0800

    wip
    Change-Id: Ibf46a747ea695503379fc6415633c4374378acf2

--- packages/web-ui-core/src/parse_history.ts
+++ packages/web-ui-core/src/parse_history.ts
@@ -14,12 +14,89 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import {
+  DamageEM,
   flattenPbOneof,
+  ModifyEntityVarEM,
   PbPhaseType,
   type PbExposedMutation,
   type PbGameState,
 } from "@gi-tcg/typings";
-import type { HistoryBlock, UseSkillHistoryBlock } from "./history";
+import type {
+  DamageHistoryChild,
+  HistoryBlock,
+  UseSkillHistoryBlock,
+  VariableChangeHistoryChild,
+} from "./history";
+
+class VariableRecord {
+  private _current: number;
+  constructor(private _init: number = 0) {
+    this._current = _init;
+  }
+  set(current: number) {
+    this._current = current;
+  }
+  take() {
+    const result = {
+      init: this._init,
+      current: this._current,
+    };
+    this._init = this._current;
+    return result;
+  }
+}
+
+/**
+ * 收集所有的 ModifyEntityVarEM。
+ * - 将生命值和 Aura 与 DamageEM 合并为 DamageHistoryChild
+ * - 将 Entity 的可见变量，角色的能量值、最大生命值记录为 VariableChangeHistoryChild
+ */
+class VariableRecorder {
+  readonly visibleVarRecords = new Map<number, VariableRecord>();
+  readonly healthVarRecords = new Map<number, VariableRecord>();
+  readonly auraVarRecords = new Map<number, VariableRecord>();
+  readonly energyVarRecords = new Map<number, VariableRecord>();
+  readonly maxHealthVarRecords = new Map<number, VariableRecord>();
+  readonly visibleVarNames = new Map<number, string>();
+
+  constructor(private readonly previousState: PbGameState | undefined) {
+    if (!previousState) {
+      return;
+    }
+    for (const player of previousState.player) {
+      for (const ch of player.character) {
+        this.healthVarRecords.set(ch.id, new VariableRecord(ch.health));
+        this.auraVarRecords.set(ch.id, new VariableRecord(ch.aura));
+        this.energyVarRecords.set(ch.id, new VariableRecord(ch.energy));
+        this.maxHealthVarRecords.set(ch.id, new VariableRecord(ch.maxHealth));
+        for (const entity of ch.entity) {
+          if (entity.variableName) {
+            this.visibleVarNames.set(entity.id, entity.variableName);
+            this.visibleVarRecords.set(
+              entity.id,
+              new VariableRecord(entity.variableValue ?? 0),
+            );
+          }
+        }
+      }
+      for (const prop of ["combatStatus", "summon", "support"] as const) {
+        for (const entity of player[prop]) {
+          if (entity.variableName) {
+            this.visibleVarNames.set(entity.id, entity.variableName);
+            this.visibleVarRecords.set(
+              entity.id,
+              new VariableRecord(entity.variableValue ?? 0),
+            );
+          }
+        }
+      }
+    }
+  }
+
+  receive(varMut: ModifyEntityVarEM): VariableChangeHistoryChild | null {}
+
+  composeDamage(dmgMut: DamageEM): DamageHistoryChild {}
+}
 
 export function parseToHistory(
   previousState: PbGameState | undefined,
@@ -34,6 +111,7 @@ export function parseToHistory(
     }
     currentSkillBlock = null;
   };
+  const varRecorder = new VariableRecorder(previousState);
   for (const pbm of mutations) {
     const m = flattenPbOneof(pbm.mutation!);
     switch (m.$case) {
@@ -57,6 +135,18 @@ export function parseToHistory(
           roundNumber: previousState?.roundNumber ?? 0,
           newPhase,
         });
+        break;
+      }
+      case "resetDice": {
+        // TODO
+        break;
+      }
+      case "modifyEntityVar": {
+        varRecorder.receive(m);
+        break;
+      }
+      case "damage": {
+        break;
       }
     }
   }
