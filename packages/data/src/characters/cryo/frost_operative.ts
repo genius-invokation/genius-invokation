f79ad9133249bb2f81e8ed434091f5520a8f053f
Guyutongxue <guyutongxue@163.com>
2025-06-20 22:33:45 +0800

    hotfix(data): 我是傻逼

--- packages/data/src/characters/cryo/frost_operative.ts
+++ packages/data/src/characters/cryo/frost_operative.ts
@@ -91,7 +91,7 @@ export const BloodbondedShadow = skill(21044)
     if (c.self.hasEquipment(RimeflowRapier)) {
       const bondSt = c.$(`status with definition id ${BondOfLife} at opp active`);
       if (bondSt) {
-        const bondValue = Math.min((1 << 31) - 1, bondSt.getVariable("usage") * 2);
+        const bondValue = Math.min(2 ** 31 - 1, bondSt.getVariable("usage") * 2);
         bondSt.setVariable("usage", bondValue);
       }
     }
