0ab68037adebaf103ff3fa9deb88012bdfd0a148
Guyutongxue <guyutongxue@163.com>
2025-07-13 17:27:51 +0800

    f

--- packages/data/src/cards/support/item.ts
+++ packages/data/src/cards/support/item.ts
@@ -128,7 +128,6 @@ const CardPlayedExtension = extension(323006, { played: pair(new Set<number>()) 
     if (e.isPlayCard()) {
       st.played[e.who].add(e.action.skill.caller.definition.id);
     }
-    // console.log(st);
   })
   .done();
 
