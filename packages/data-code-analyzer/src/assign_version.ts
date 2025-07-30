1604135f1978952215e26b3c41ebbee1e9a9a267
Guyutongxue <guyutongxue@163.com>
2025-07-30 10:47:55 +0800

    feat(data-code-analyzer): A TS source analyzer for `@gi-tcg/data` (#228)
    * feat(data-code-analyzer): An TS source analyzer for `@gi-tcg/data`
    
    * no
    
    Change-Id: Ie82dce3d508d8ee3b60ec09f07acd1e0968a24f6
    
    * no
    
    Change-Id: Ie9c4ca19f4444cbb746144afdaf4ba0d4c8f7f05
    
    * lockfile fix
    
    * oo
    
    * update
    
    * update
    
    * add moyucup
    
    * done
    
    Change-Id: Ie9f34db7f1a25a6ae25fb7adefe83952f15c044c

---
+++ packages/data-code-analyzer/src/assign_version.ts
@@ -0,0 +1,61 @@
+import { CURRENT_VERSION, Version } from "@gi-tcg/core";
+import entityDependency_ from "./deps.json" with { type: "json" };
+import { MOYU_S7_VERSIONS } from "./moyu_s7_version";
+const entityDependency = new Map<number, number[]>(
+  Object.entries(entityDependency_ as Record<number, number[]>).map(
+    ([k, v]) => [Number(k), v] as const,
+  ),
+);
+
+interface VersionAssignment {
+  strong?: Version;
+  weak?: Version;
+  weakFromIds?: number[];
+}
+
+export const assignVersion = (
+  baseVersion: Version,
+  versions: Record<number, Version>,
+): Record<number, Version> => {
+  const result = new Map<number, VersionAssignment>();
+  for (const [id] of entityDependency) {
+    result.set(id, {
+      strong: versions[id],
+    });
+  }
+  const applyWeak = (id: number, version: Version, fromIds: number[]) => {
+    const et = result.get(id);
+    if (!et) {
+      return;
+    }
+    if (et.strong) {
+      return;
+    }
+    if (fromIds.includes(id)) {
+      return;
+    }
+    if (!et.weak) {
+      et.weak = version;
+      et.weakFromIds = fromIds;
+    } else if (et.weak !== version) {
+      throw new Error(
+        `Entity ${id} has conflicting weak versions: ${et.weak} (from ${et.weakFromIds}) vs ${version} (from ${fromIds})`,
+      );
+    }
+    for (const subId of entityDependency.get(id) ?? []) {
+      applyWeak(subId, version, [...fromIds, id]);
+    }
+  };
+  for (const [id, version] of Object.entries(versions)) {
+    applyWeak(Number(id), version, []);
+  }
+  return Object.fromEntries(
+    result.entries().map(([id, { strong, weak }]) => {
+      return [id, strong ?? weak ?? baseVersion];
+    }),
+  );
+};
+
+const assignResult = assignVersion(CURRENT_VERSION, MOYU_S7_VERSIONS);
+// @ts-expect-error
+assignResult["$base"] = CURRENT_VERSION;
