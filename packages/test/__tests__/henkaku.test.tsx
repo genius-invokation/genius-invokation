84943b40a948d2c18020f053cf35af4c97804b67
DrAbx123 <79169815+DrAbx123@users.noreply.github.com>
2025-09-11 23:58:27 +0800

    test: declension layers when using e

---
+++ packages/test/__tests__/henkaku.test.tsx
@@ -0,0 +1,23 @@
+import { ref, setup, Character, State, Status, Equipment, Summon, CombatStatus } from "#test";
+import { ShikanoinHeizou, Declension } from "@gi-tcg/data/internal/characters/anemo/shikanoin_heizou";
+import { AurousBlaze } from "@gi-tcg/data/internal/characters/pyro/yoimiya"
+import { Aura } from "@gi-tcg/typings";
+import { test } from "bun:test";
+
+test("declension layers when using e", async () => {
+  const oppActive = ref();
+  const declension = ref();
+  const c = setup(
+    <State>
+        <Character opp active ref={oppActive} />
+        <Character my def={ShikanoinHeizou} >
+            <Status def={Declension} ref={declension} v={{ henkaku: 5 }} />
+        </Character>
+        <CombatStatus my def={AurousBlaze} />
+    </State>,
+  );
+
+  await c.me.skill(HeartstopperStrike)
+  c.expect(declension).toHaveVariable({ henkaku: 4 });
+  c.expect(oppActive).toHaveVariable({ aura: Aura.None });
+});
