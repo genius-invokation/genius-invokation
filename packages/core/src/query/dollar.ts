cf93c400a3932043f41a453272d15c2806f680dd
Guyutongxue <guyutongxue@163.com>
2025-10-12 23:53:39 +0800

    1

--- packages/core/src/query/dollar.ts
+++ packages/core/src/query/dollar.ts
@@ -1,12 +1,48 @@
 
 
-class Query {
+type Expression = string | number | ExpressionArray;
+interface ExpressionArray extends Array<Expression> {}
+
+const toExpression: unique symbol = Symbol("toExpression");
+
+abstract class Query {
+  protected abstract readonly type: string;
+  abstract [toExpression](): Expression;
+}
+
+class PrimaryQuery extends Query {
+  protected readonly type: "primary";
+  constructor() {
+    super();
+    this.type = "primary";
+  }
+
   get my() {
     return this;
   }
   get opp() {
     return this;
   }
+  [toExpression](): Expression {
+    // TODO
+    return [];
+  }
 }
 
-const $ = new Query();
+type CompositeOperator = "not" | "recentFrom" | "has" | "at" | "orElse" | "union" | "intersection"
+
+class CompositeQuery extends Query {
+  protected readonly type: CompositeOperator;
+  constructor(type: CompositeOperator) {
+    super();
+    this.type = type;
+  }
+  [toExpression](): Expression {
+    // TODO
+    return [this.type];
+  }
+}
+
+const $ = new PrimaryQuery();
+
+$.my;
