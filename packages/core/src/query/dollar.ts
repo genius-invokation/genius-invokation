1cdc878369fb1a222e70f266d7c48ed02d6c84f6
Guyutongxue <guyutongxue@163.com>
2025-10-14 00:28:04 +0800

    add gemini code

--- packages/core/src/query/dollar.ts
+++ packages/core/src/query/dollar.ts
@@ -1,48 +1,53 @@
-
-
 type Expression = string | number | ExpressionArray;
 interface ExpressionArray extends Array<Expression> {}
 
 const toExpression: unique symbol = Symbol("toExpression");
 
-abstract class Query {
-  protected abstract readonly type: string;
-  abstract [toExpression](): Expression;
+interface IQuery {
+  [toExpression]: () => Expression;
 }
 
-class PrimaryQuery extends Query {
-  protected readonly type: "primary";
-  constructor() {
-    super();
-    this.type = "primary";
-  }
-
+class PrimaryMethods {
   get my() {
     return this;
   }
   get opp() {
     return this;
   }
+}
+
+class Dollar extends PrimaryMethods {}
+
+class PrimaryQuery extends PrimaryMethods implements IQuery {
+  protected readonly type: "primary";
+  constructor() {
+    super();
+    this.type = "primary";
+  }
+
   [toExpression](): Expression {
     // TODO
     return [];
   }
 }
 
-type CompositeOperator = "not" | "recentFrom" | "has" | "at" | "orElse" | "union" | "intersection"
+type CompositeOperator =
+  | "not"
+  | "recentFrom"
+  | "has"
+  | "at"
+  | "orElse"
+  | "union"
+  | "intersection";
 
-class CompositeQuery extends Query {
-  protected readonly type: CompositeOperator;
-  constructor(type: CompositeOperator) {
-    super();
-    this.type = type;
-  }
+class CompositeQuery implements IQuery {
+  constructor(private readonly type: CompositeOperator) {}
   [toExpression](): Expression {
     // TODO
     return [this.type];
   }
 }
 
-const $ = new PrimaryQuery();
+declare const $: Dollar;
 
 $.my;
