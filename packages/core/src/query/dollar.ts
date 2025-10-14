1a237f20c8a5d36138bca197a2fd7ca18e5249d7
Guyutongxue <guyutongxue@163.com>
2025-10-14 23:23:55 +0800

    1

--- packages/core/src/query/dollar.ts
+++ packages/core/src/query/dollar.ts
@@ -7,22 +7,100 @@ interface IQuery {
   [toExpression]: () => Expression;
 }
 
-class PrimaryMethods {
-  get my() {
-    return this;
+class PrimaryMethods<RetT = unknown> {
+  #self: RetT = this as unknown as RetT;
+  // who
+  get my(): RetT {
+    return this.#self;
   }
-  get opp() {
-    return this;
+  get opp(): RetT {
+    return this.#self;
+  }
+  get all(): RetT {
+    return this.#self;
+  }
+  // type/area
+  get character(): RetT {
+    return this.#self;
+  }
+  get equipment(): RetT {
+    return this.#self;
+  }
+  get status(): RetT {
+    return this.#self;
+  }
+  get summon(): RetT {
+    return this.#self;
+  }
+  get support(): RetT {
+    return this.#self;
+  }
+  get hand(): RetT {
+    return this.#self;
+  }
+  get pile(): RetT {
+    return this.#self;
+  }
+  get card(): RetT {
+    return this.#self;
+  }
+  // position
+  get active(): RetT {
+    return this.#self;
+  }
+  get prev(): RetT {
+    return this.#self;
+  }
+  get next(): RetT {
+    return this.#self;
+  }
+  get standby(): RetT {
+    return this.#self;
+  }
+  // defeated
+  get onlyDefeated(): RetT {
+    return this.#self;
+  }
+  get includesDefeated(): RetT {
+    return this.#self;
+  }
+  // with
+  // TODO
+  var(...args: unknown[]): RetT {
+    return this.#self;
+  }
+  id(id: number): RetT {
+    return this.#self;
+  }
+  def(id: number): RetT {
+    return this.#self;
+  }
+  tag(...tags: string[]): RetT {
+    return this.#self;
+  }
+  tagOf(type: unknown, query: unknown): RetT {
+    return this.#self;
   }
 }
 
-class Dollar extends PrimaryMethods {}
+type PrimaryMethodNames = keyof PrimaryMethods & {};
 
-class PrimaryQuery extends PrimaryMethods implements IQuery {
-  protected readonly type: "primary";
+const PRIMARY_METHODS = Object.getOwnPropertyDescriptors(
+  PrimaryMethods.prototype,
+) as {
+  [K in PrimaryMethodNames]: PropertyDescriptor;
+} & {
+  ["constructor"]?: unknown;
+};
+delete PRIMARY_METHODS.constructor;
+
+const PRIMARY_METHOD_NAMES = Object.keys(PRIMARY_METHODS) as PrimaryMethodNames[];
+
+type Dollar = PrimaryMethods<PrimaryQuery>;
+
+class PrimaryQuery extends PrimaryMethods<PrimaryQuery> implements IQuery {
   constructor() {
     super();
-    this.type = "primary";
   }
 
   [toExpression](): Expression {
@@ -50,4 +128,4 @@ class CompositeQuery implements IQuery {
 
 declare const $: Dollar;
 
-$.my;
+$.my.my.my;
