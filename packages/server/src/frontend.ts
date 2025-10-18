6c1234f8556aca5037729cf69821c3c9a6f26a85
Guyutongxue <guyutongxue@163.com>
2025-10-19 00:31:48 +0800

    fixes

--- packages/server/src/frontend.ts
+++ packages/server/src/frontend.ts
@@ -14,44 +14,38 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import mime from "mime";
-import type { FastifyInstance } from "fastify";
+import type { FastifyInstance, RouteHandlerMethod } from "fastify";
 import fastifyEtag from "@fastify/etag";
 import { WEB_CLIENT_BASE_PATH } from "@gi-tcg/config";
 
 export async function frontend(app: FastifyInstance) {
-  await app.register(fastifyEtag as any);
+  await app.register(fastifyEtag);
 
   if (process.env.NODE_ENV === "production") {
-    const { default: contents } = await import("@gi-tcg/web-client");
-    const baseNoSuffix = WEB_CLIENT_BASE_PATH.replace(/(.+)\/$/, "$1");
-    const NO_CACHE = "public, no-cache, must-revalidate";
-
-    const { "index.html": indexHtml, ...rest } = contents;
+    const {
+      default: { "index.html": indexHtml, ...rest },
+    } = await import("@gi-tcg/web-client");
 
     for (const [name, content] of Object.entries(rest)) {
       const buffer = Buffer.from(content, "base64");
+      const type = mime.getType(name) ?? "application/octet-stream";
       app.get(`${WEB_CLIENT_BASE_PATH}${name}`, (_req, reply) => {
         reply
           .header("Cache-Control", "public, max-age=31536000, immutable")
-          .type(mime.getType(name) ?? "application/octet-stream")
+          .type(type)
           .send(buffer);
       });
     }
 
     const indexHtmlBuffer = Buffer.from(indexHtml!, "base64");
-
-    app.get(baseNoSuffix, (_req, reply) => {
-      reply
-        .header("Cache-Control", NO_CACHE)
+    const indexHtmlHandler: RouteHandlerMethod = (_req, reply) => {
+      return reply
+        .header("Cache-Control", "public, no-cache, must-revalidate")
         .type("text/html")
         .send(indexHtmlBuffer);
-    });
-
-    app.get(`${WEB_CLIENT_BASE_PATH}*`, (_req, reply) => {
-      reply
-        .header("Cache-Control", NO_CACHE)
-        .type("text/html")
-        .send(indexHtmlBuffer);
-    });
+    };
+    const baseNoSuffix = WEB_CLIENT_BASE_PATH.replace(/(.+)\/$/, "$1");
+    app.get(baseNoSuffix, indexHtmlHandler);
+    app.get(`${WEB_CLIENT_BASE_PATH}*`, indexHtmlHandler);
   }
 }
