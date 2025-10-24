c260944e52a5cdd7bdeafcf6d662d503c7635cf2
Guyutongxue <guyutongxue@163.com>
2025-10-24 17:11:35 +0800

    union & intersection
    Change-Id: Iff87b9442956f69cbf5fa6f96b3778dedafac918

--- packages/core/src/query/dollar.ts
+++ packages/core/src/query/dollar.ts
@@ -1,4 +1,5 @@
-import type { EntityArea } from "../base/entity";
+import type { CardTag, CharacterTag } from "..";
+import type { EntityArea, EntityTag, EntityType } from "../base/entity";
 import type {
   CardHandle,
   CharacterHandle,
@@ -12,7 +13,9 @@ import type {
 
 type IsExtends<T, U> = T extends U ? true : false;
 type Related<T, U> = T extends U ? true : U extends T ? true : false;
-type Computed<T> = { [K in keyof T]: T[K] } extends infer O ? O : never;
+type Computed<T, R = any> = { [K in keyof T]: T[K] } extends infer O extends R
+  ? O
+  : never;
 type StaticAssert<T extends true> = T;
 type NotFunctionPrototype = {
   /** @deprecated This object do not have function prototype */
@@ -39,17 +42,20 @@ type Expression = string | number | ExpressionArray;
 interface ExpressionArray extends Array<Expression> {}
 
 const toExpression: unique symbol = Symbol("toExpression");
-const meta: unique symbol = Symbol("meta");
+const retMeta: unique symbol = Symbol("meta");
 
-type MetaSymbol = typeof meta;
+type RetMeta = typeof retMeta;
 
-interface IQuery {
-  [meta]: MetaBase;
+interface IQuery<Meta extends MetaBase = MetaBase> {
+  [retMeta]: Meta;
   [toExpression]: () => Expression;
 }
 
-type InferResult<Q extends IQuery> = ReturnOfMeta<Q[MetaSymbol]>;
+type InferResult<Q extends IQuery> = Computed<Q[RetMeta], MetaBase>;
 
+type HeterogeneousMetaBase = MetaBase & {
+  returns: "identical" | MetaBase;
+};
 type MetaBase = {
   type: ExEntityType;
   areaType: EntityArea["type"];
@@ -58,16 +64,20 @@ type MetaBase = {
   position: "active" | "prev" | "next" | "standby";
   defeated: "only" | "includes";
   id: number;
-  returns: "identical" | MetaBase;
 };
 
-type ReturnOfMeta<M extends MetaBase> = M["returns"] extends "identical"
-  ? Computed<M>
-  : M["returns"] extends MetaBase
-    ? ReturnOfMeta<M["returns"]>
-    : never;
+type ReturnOfMeta<M extends MetaBase> = Computed<
+  M extends HeterogeneousMetaBase
+    ? M["returns"] extends "identical"
+      ? Omit<M, "returns"> & { returns: "identical" }
+      : M["returns"] extends MetaBase
+        ? M["returns"]
+        : never
+    : M,
+  MetaBase
+>;
 
-type PositionPatch<T extends MetaBase["position"]> = {
+type PositionPatch<T extends HeterogeneousMetaBase["position"]> = {
   type: "character";
   areaType: "characters";
   position: T;
@@ -76,44 +86,55 @@ type PositionPatch<T extends MetaBase["position"]> = {
 type DefPatch<T extends HandleT<ExEntityType>> = (T extends EquipmentHandle
   ? {
       type: "equipment";
-      area: "characters";
+      areaType: "characters";
     }
   : T extends SupportHandle
     ? {
         type: "support";
-        area: "supports";
+        areaType: "supports";
       }
     : T extends StatusHandle
       ? {
           type: "status";
-          area: "characters";
+          areaType: "characters";
         }
       : T extends SummonHandle
         ? {
             type: "summon";
-            area: "summons";
+            areaType: "summons";
           }
         : T extends CharacterHandle
           ? {
               type: "character";
-              area: "characters";
+              areaType: "characters";
             }
           : T extends CardHandle
             ? {
                 type: "card";
+                areaType: "hands" | "pile";
               }
             : {}) & {
   definition: T & { readonly _defSpecified: unique symbol };
 };
 
-type Assign<
-  T extends MetaBase,
-  Patch extends Partial<MetaBase> = {},
-> = RestrictedPrimaryQuery<
-  Computed<T & Patch> extends MetaBase ? Computed<T & Patch> : never
->;
+type TagOfImpl<Ty extends ExEntityType> = Ty extends EntityType
+  ? EntityTag
+  : Ty extends "character"
+    ? CharacterTag
+    : Ty extends "card"
+      ? CardTag
+      : never;
 
-class PrimaryMethods<Meta extends MetaBase> {
+type TagOf<Meta extends HeterogeneousMetaBase> = {
+  [K in Meta["type"]]: TagOfImpl<K>;
+}[Meta["type"]];
+
+type Assign<
+  T extends HeterogeneousMetaBase,
+  Patch extends Partial<HeterogeneousMetaBase> = {},
+> = RestrictedPrimaryQuery<Computed<T & Patch, HeterogeneousMetaBase>>;
+
+class PrimaryMethods<Meta extends HeterogeneousMetaBase> {
   private _self: any = this;
   // who
   get my(): Assign<Meta, { who: "my" }> {
@@ -197,7 +218,7 @@ class PrimaryMethods<Meta extends MetaBase> {
   def(id: number): unknown {
     return this._self;
   }
-  tag(...tags: string[]): Assign<Meta> {
+  tag(...tags: TagOf<Meta>[]): Assign<Meta> {
     return this._self;
   }
   tagOf(type: unknown, query: unknown): Assign<Meta> {
@@ -262,25 +283,27 @@ type DollarUnaryOperatorMethods = {
   [K in keyof UnaryOperatorMetas as UnaryOperatorMetas[K]["name"]]: {
     <T extends IQuery>(
       arg: RelatedToMetaReq<
-        T[MetaSymbol],
+        InferResult<T>,
         UnaryOperatorMetas[K]["operand"]
       > extends true
         ? T
         : never,
-    ): RestrictedPrimaryQuery<UnaryOperatorMetas[K]["result"] & InitialMeta>;
+    ): RestrictedPrimaryQuery<
+      UnaryOperatorMetas[K]["result"] & HeterogeneousMetaBase
+    >;
   } & RestrictedPrimaryQuery<
-    Omit<InitialMeta & UnaryOperatorMetas[K]["operand"], "returns"> & {
-      returns: UnaryOperatorMetas[K]["result"] & InitialMeta;
+    Omit<MetaBase & UnaryOperatorMetas[K]["operand"], "returns"> & {
+      returns: UnaryOperatorMetas[K]["result"] & MetaBase;
     }
   > &
     NotFunctionPrototype;
 };
 
-class PrimaryQuery<Meta extends MetaBase>
+class PrimaryQuery<Meta extends HeterogeneousMetaBase>
   extends PrimaryMethods<Meta>
-  implements IQuery
+  implements IQuery<ReturnOfMeta<Meta>>
 {
-  declare [meta]: Meta;
+  declare [retMeta]: ReturnOfMeta<Meta>;
 
   constructor(private _leadingUnaryOp: UnaryOperator | null) {
     super();
@@ -317,7 +340,7 @@ type _Check1 = StaticAssert<
   IsExtends<
     RestrictionConfig,
     {
-      [K in keyof RestrictionConfig]: Partial<MetaBase>;
+      [K in keyof RestrictionConfig]: Partial<HeterogeneousMetaBase>;
     }
   >
 >;
@@ -340,13 +363,13 @@ type NotStrictlySuperTypeOf<T, U> = StrictlySuperTypeOf<T, U> extends true
   : true;
 type PickingPropsNotStrictlySuperTypeOf<
   Meta,
-  ConfigMeta extends Partial<MetaBase>,
+  ConfigMeta extends Partial<HeterogeneousMetaBase>,
 > = NotStrictlySuperTypeOf<
   Pick<Meta, keyof Meta & keyof ConfigMeta>,
   ConfigMeta
 >;
 
-type OmittedMethods<Meta extends MetaBase> = {
+type OmittedMethods<Meta extends HeterogeneousMetaBase> = {
   [K in PrimaryMethodNames]: K extends keyof RestrictionConfig
     ? PickingPropsNotStrictlySuperTypeOf<
         Meta,
@@ -365,14 +388,39 @@ type OmittedMethods<Meta extends MetaBase> = {
         : never;
 }[PrimaryMethodNames];
 
-type RestrictedPrimaryQuery<Meta extends MetaBase> = Omit<
+type RestrictedPrimaryQuery<Meta extends HeterogeneousMetaBase> = Omit<
   PrimaryQuery<Meta>,
   OmittedMethods<Meta>
 >;
 
-class CompositeQuery implements IQuery {
-  declare [meta]: MetaBase; // TODO
-  constructor(private readonly type: CompositeOperator) {}
+type UnionMeta2<Meta1 extends MetaBase, Meta2 extends MetaBase> = {
+  [K in keyof Meta1 & keyof Meta2]: Meta1[K] | Meta2[K];
+};
+type UnionMeta<Metas extends MetaBase[]> = Computed<
+  Metas extends [infer First extends MetaBase, ...infer Rest extends MetaBase[]]
+    ? UnionMeta2<First, UnionMeta<Rest>>
+    : never,
+  MetaBase
+>;
+
+type IntersectionMeta2<Meta1 extends MetaBase, Meta2 extends MetaBase> = {
+  [K in keyof Meta1 & keyof Meta2]: Meta1[K] & Meta2[K];
+};
+type IntersectionMeta<Metas extends MetaBase[]> = Computed<
+  Metas extends [infer First extends MetaBase, ...infer Rest extends MetaBase[]]
+    ? IntersectionMeta2<First, IntersectionMeta<Rest>>
+    : MetaBase,
+  MetaBase
+>;
+
+class CompositeQuery<Meta extends MetaBase>
+  implements IQuery<ReturnOfMeta<Meta>>
+{
+  declare [retMeta]: ReturnOfMeta<Meta>;
+  constructor(
+    private readonly type: CompositeOperator,
+    private readonly operands: IQuery[],
+  ) {}
   [toExpression](): Expression {
     // TODO
     return [this.type];
@@ -380,18 +428,53 @@ class CompositeQuery implements IQuery {
 }
 
 class Dollar {
-  // get has() {
-  //   return new PrimaryQuery<EntityOnCharacterMetaBase>("unaryHas");
-  // }
-  // get at() {
-  //   return new PrimaryQuery<CharacterMetaBase>("unaryAt");
-  // }
-  // get not() {
-  //   return new PrimaryQuery<MetaBase>(`not`);
-  // }
-  // get recentFrom() {
-  //   return new PrimaryQuery<CharacterMetaBase>("recentFrom");
-  // }
+  intersection<A extends MetaBase, B extends MetaBase>(
+    a: IQuery<A>,
+    b: IQuery<B>,
+  ): CompositeQuery<IntersectionMeta<[A, B]>>;
+  intersection<A extends MetaBase, B extends MetaBase, C extends MetaBase>(
+    a: IQuery<A>,
+    b: IQuery<B>,
+    c: IQuery<C>,
+  ): CompositeQuery<IntersectionMeta<[A, B, C]>>;
+  intersection<
+    A extends MetaBase,
+    B extends MetaBase,
+    C extends MetaBase,
+    D extends MetaBase,
+  >(
+    a: IQuery<A>,
+    b: IQuery<B>,
+    c: IQuery<C>,
+    d: IQuery<D>,
+  ): CompositeQuery<IntersectionMeta<[A, B, C, D]>>;
+  intersection(...args: IQuery[]): CompositeQuery<any> {
+    return new CompositeQuery("intersection", args);
+  }
+
+  union<A extends MetaBase, B extends MetaBase>(
+    a: IQuery<A>,
+    b: IQuery<B>,
+  ): CompositeQuery<UnionMeta<[A, B]>>;
+  union<A extends MetaBase, B extends MetaBase, C extends MetaBase>(
+    a: IQuery<A>,
+    b: IQuery<B>,
+    c: IQuery<C>,
+  ): CompositeQuery<UnionMeta<[A, B, C]>>;
+  union<
+    A extends MetaBase,
+    B extends MetaBase,
+    C extends MetaBase,
+    D extends MetaBase,
+  >(
+    a: IQuery<A>,
+    b: IQuery<B>,
+    c: IQuery<C>,
+    d: IQuery<D>,
+  ): CompositeQuery<UnionMeta<[A, B, C, D]>>;
+  union(...args: IQuery[]): CompositeQuery<any> {
+    return new CompositeQuery("union", args);
+  }
 }
 
 for (const [method, descriptor] of Object.entries<PropertyDescriptor>(
@@ -436,7 +519,7 @@ for (const [operator, name] of Object.entries(UNARY_OPS) as [
 ][]) {
   const chainForm = () => {
     const callingForm = (q: IQuery) => {
-      return new CompositeQuery(operator);
+      return new PrimaryQuery(operator);
     };
     const returns = new PrimaryQuery(operator);
     Object.setPrototypeOf(callingForm, returns);
@@ -448,16 +531,16 @@ for (const [operator, name] of Object.entries(UNARY_OPS) as [
   });
 }
 
-type InitialMeta = Computed<
-  Omit<MetaBase, "returns"> & { returns: "identical" }
->;
-
 type IDollar = Dollar &
-  PrimaryMethods<InitialMeta> &
+  PrimaryMethods<MetaBase & { returns: "identical" }> &
   DollarUnaryOperatorMethods;
 
 const $ = new Dollar() as IDollar;
 
-const x = $.my.active;
+const x = $.intersection(
+  $.opp,
+  $.union($.status, $.combatStatus, $.summon),
+  $.union($.tag("barrier"), $.tag("shield")),
+);
 
 type X = InferResult<typeof x>;
