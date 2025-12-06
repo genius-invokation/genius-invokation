f1d75a4c62705c5400b75414bb120e86aba916c5
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-12-07 02:45:28 +0800

    feat(server): remote dev

--- packages/server/src/dev.ts
+++ packages/server/src/dev.ts
@@ -1,7 +1,11 @@
 import { unstable_startServer } from "@prisma/dev";
 import { $ } from "bun";
-import path from "path";
 import getPort from "get-port";
+import path from "path";
+
+if (process.env.NODE_ENV === "production") {
+  throw new Error("Dev server should not be started in production mode");
+}
 
 async function startLocalPrisma(name: string) {
   const port = await getPort();
@@ -13,16 +17,34 @@ async function startLocalPrisma(name: string) {
   });
 }
 
-const server = await startLocalPrisma("gi-tcg-server-dev");
-try {
-  await $`bunx prisma migrate dev`.env({ DATABASE_URL: server.ppg.url });
+async function localDev() {
+  const server = await startLocalPrisma("gi-tcg-server-dev");
+  try {
+    await $`bunx prisma migrate dev`.env({ DATABASE_URL: server.ppg.url });
+    await $`bunx prisma generate`;
+    await $`bun --watch ${path.resolve(import.meta.dirname, "main.ts")}`
+      .env({
+        DATABASE_URL: server.database.connectionString,
+        DATABASE_CONNECTION_LIMIT: "1",
+      })
+      .nothrow();
+  } finally {
+    await server.close!();
+  }
+}
+
+async function remoteDev() {
+  await $`bunx prisma migrate dev`.env({
+    DATABASE_URL: process.env.DATABASE_URL!,
+  });
   await $`bunx prisma generate`;
   await $`bun --watch ${path.resolve(import.meta.dirname, "main.ts")}`
-    .env({
-      DATABASE_URL: server.database.connectionString,
-      DATABASE_CONNECTION_LIMIT: "1",
-    })
+    .env({ DATABASE_URL: process.env.DATABASE_URL! })
     .nothrow();
-} finally {
-  await server.close!();
+}
+
+if (process.env.DATABASE_URL) {
+  await remoteDev();
+} else {
+  await localDev();
 }
